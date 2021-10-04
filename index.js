93a9cfe9f81c5382e2fae3bf1825354a8612cebf
RXWiki <59118348+RXWiki@users.noreply.github.com>
2021-10-04 16:31:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -408,7 +408,7 @@ bot.hears('сильно пнуть', (ctx) => {
 bot.hears('ударить топором', (ctx) => {
     let id = ctx.message.from.id
     let id2 = ctx.message.reply_to_message.from.id
-    if(ctx.message.from.id === arr[i]) {
+    if(ctx.message.from.id === 1147885120) {
         ctx.reply(`Принтеру не засунуть`) } else {
     ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name}</a> засунул(а) топор в жопу <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name}</a>`, { 
    parse_mode: "HTML", 
