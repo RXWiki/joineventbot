c218e84a5e444c7f3079a076b8d13e706cb677e5
RXWiki <59118348+RXWiki@users.noreply.github.com>
2021-10-04 19:24:52 +0300

    Delete tutor.js

--- tutor.js
+++
@@ -1,8 +0,0 @@
-let str1 = "<Span class='my'>"
-let regex = /<(([A-z]+)\s*([^>]*))>/
-
-let result = str1.match(regex);
-console.log(result[0])
-console.log(result[1])
-console.log(result[2])
-console.log(result[3])
