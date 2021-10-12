e4676e714a8ffa4bb8603fe4a5889c7179f122c8
RXWiki <wikilodiya@gmail.com>
2021-10-12 21:26:06 +0300

    update

--- index.js
+++ index.js
@@ -1110,29 +1110,29 @@ bot.hears(/[Пп]окормить с ложечки/, (ctx) => {
 bot.hears(/[Уу]дарить топором/, (ctx) => {
     let text = ctx.message.text
     let texter = text.split(" ")
-    let tagi = /\p{sc=Cyrillic}+\n\p{sc=Cyrillic}+/gui
-    let word1 = text.match(tagi)
     let tag = /\s\p{sc=Cyrillic}+/gui
     let word = text.match(tag)
     let id = ctx.message.from.id
+    let tagi = /\p{sc=Cyrillic}+\n\p{sc=Cyrillic}+/gui
+    let word1 = text.match(tagi)
     let id2 = ctx.message.reply_to_message.from.id
     let tager = /[\p{sc=Cyrillic}+.,?!\s\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/ug
-    try {   
-        if (texter[2] === word[1] && word[1] !== undefined || texter[1] === word[0] && word !== undefined) {
-            ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> засунул(а) топор в жопу к <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}
+    try {
+        if (texter[2] === word[1] && word[1] !== undefined  || texter[1] === word1[0] && word1 !== null) {
+            ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> засунул(а) топор в жопу <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}
 </a>Сказав: ${ctx.message.text.match(tager).join("").slice(16,)}`, {
-                parse_mode: "HTML", 
+                parse_mode: "HTML",
                 disable_web_page_preview: true
             })
         } else {
-            ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> засунул(а) топор в жопу к <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}
-</a>`, {
-                parse_mode: "HTML",
-                disable_web_page_preview: true
-        })}
+            console.log('f')
+            ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> засунул(а) топор в жопу <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}</a>`, {
+            parse_mode: "HTML",
+            disable_web_page_preview: true
+        })
+        }
     } catch (err) {
-        console.log(err)
-        ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> засунул(а) топор в жопу к <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}</a>`, {
+        ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> засунул(а) топор в жопу <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}</a>`, {
             parse_mode: "HTML",
             disable_web_page_preview: true
         })
@@ -1141,31 +1141,28 @@ bot.hears(/[Уу]дарить топором/, (ctx) => {
 bot.hears(/[Кк]упить булочку/, (ctx) => {
     let text = ctx.message.text
     let texter = text.split(" ")
-    let tagi = /\p{sc=Cyrillic}+\n\p{sc=Cyrillic}+/gui
-    let word1 = text.match(tagi)
     let tag = /\s\p{sc=Cyrillic}+/gui
     let word = text.match(tag)
     let id = ctx.message.from.id
+    let tagi = /\p{sc=Cyrillic}+\n\p{sc=Cyrillic}+/gui
+    let word1 = text.match(tagi)
     let id2 = ctx.message.reply_to_message.from.id
     let tager = /[\p{sc=Cyrillic}+.,?!\s\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/ug
-    console.log(" ")
-    console.log(texter[2])
-    console.log(word[1])
-    try {   
-        if (texter[2] === word[1] && word[1] !== undefined || texter[1] === word[0] && word !== undefined) {
+    try {
+        if (texter[2] === word[1] && word[1] !== undefined  || texter[1] === word1[0] && word1 !== null) {
             ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> купил(а) булочку для <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}
 </a>Сказав: ${ctx.message.text.match(tager).join("").slice(15,)}`, {
-                parse_mode: "HTML", 
+                parse_mode: "HTML",
                 disable_web_page_preview: true
             })
         } else {
-            ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> купил(а) булочку для <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}
-</a>`, {
-                parse_mode: "HTML",
-                disable_web_page_preview: true
-        })}
+            console.log('f')
+            ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> купил(а) булочку для <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}</a>`, {
+            parse_mode: "HTML",
+            disable_web_page_preview: true
+        })
+        }
     } catch (err) {
-        console.log(err)
         ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> купил(а) булочку для <a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}</a>`, {
             parse_mode: "HTML",
             disable_web_page_preview: true
