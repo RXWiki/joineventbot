fbc655bb75480699d91d8b81dd2db7f75a5ea253
RXWiki <59118348+RXWiki@users.noreply.github.com>
2022-06-28 21:51:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1210,7 +1210,7 @@ bot.hears(/[Пп]ригласить в бс/, (ctx) => {
     let id2 = ctx.message.reply_to_message.from.id
     let tager = /[\p{sc=Cyrillic}+.,?!\s\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/ug
     try {
-        if (texter[2] === word[1] && word[1] !== undefined  || texter[1] === word1[0] && word1 !== null) {
+        if (texter[3] === word[2] && word[2] !== undefined || texter[2] === word1[0] && word1 !== null) {
             ctx.reply(`<a href='tg://user?id=${id}'>${ctx.message.from.first_name.replace(/[><]/g, '')}</a> пригласил(а) поиграть в бравл старс<a href='tg://user?id=${id2}'>${ctx.message.reply_to_message.from.first_name.replace(/[><]/g, '')}
 </a>Сказав: ${ctx.message.text.match(tager).join("").slice(15,)}`, {
                 parse_mode: "HTML",
